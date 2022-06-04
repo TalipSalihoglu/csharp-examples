b1d73b28e8a938e32355a47c97f65f1b655f59fe
TalipSalihoglu <salihoglutalip@gmail.com>
2022-06-04 16:58:06 +0300

    Initial commit

---
+++ IntermediateOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace IntermediateOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch watch = new StopWatch();
+            watch.Start();
+            Thread.Sleep(3000);
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed);
+        }
+    }
+}
---
+++ IntermediateOOP/StopWatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IntermediateOOP
+{
+    public class StopWatch
+    {
+        private DateTime _start;
+        private TimeSpan _elapsed;
+        public TimeSpan Elapsed
+        {
+            get
+            {
+                return _elapsed;
+            }
+        }
+        public void Start()
+        {
+            if (_start != default)
+                throw new InvalidOperationException("StopWatch already started.");
+
+            _start = DateTime.Now;
+        }
+        public void Stop()
+        {
+            if (_start == default)
+                throw new InvalidOperationException("StopWatch not started.");
+
+            _elapsed = DateTime.Now - _start;
+            Reset();
+        }
+        public void Reset()
+        {
+            _start = default;
+        }
+    }
+}
